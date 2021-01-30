6e0f4c58088812adb980efb9d03295f56b342edb
Jason Madar <jmadar@gmail.com>
2021-01-29 21:54:46 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# curl-compat-cookie-extension
+
+A basic chrome extension that allows arbitrary reading and
+writing of cookies from/to any website using
+a curl compatible cookie string.
+
+Great for doing demos on how cookies work.
---
+++ fun.js
@@ -0,0 +1,45 @@
+
+function extract(domain) {
+  console.log("extracts cookies from moodle site");
+  chrome.cookies.getAll( {
+    domain: domain
+  }, (cookies) => {
+    let cookie_string = cookies.map( c => `${c.name}=${c.value}` ).join('; ');
+    console.log(cookie_string);
+    navigator.clipboard.writeText(cookie_string);
+  });
+}
+
+function writeCookie(url, cookieString) {
+  cookieString.split(';').forEach( nv => {
+    let ary = nv.trim().split('=');
+    let name = ary[0];
+    let value = ary[1];
+    chrome.cookies.set( {
+      url: url,
+      name: name,
+      value: value,
+      path: '/',
+      secure: true,
+      httpOnly: true
+    });
+  });
+}
+
+window.addEventListener('load', function() {  
+  
+  document.querySelector('#extract_button').addEventListener('click', () => {
+    let domain = document.getElementById('domain').value;
+    extract(domain);
+    alert('cookie copied to clipboard');
+  });
+  
+  document.querySelector('#write_button').addEventListener('click', () => {
+    let cookieString = document.getElementById('cookie_string').value;
+    let url = document.getElementById('url').value;    
+    writeCookie(url, cookieString);
+    alert('cookie written to browser');
+  });
+
+});
+
